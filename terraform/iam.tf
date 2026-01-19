2c0a954f21e2a37af738d99c768761196318aa55
eric m. schmidt <eric@spaceba.by>
2026-01-19 16:51:54 -0500

    Restrict GitHub OIDC trust policy to main branch only
    The previous policy allowed any branch or event type to assume the
    GitHub Actions IAM role. Now only pushes to the main branch can
    upload artifacts to S3, preventing rogue branches from deploying.

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -305,9 +305,7 @@ resource "aws_iam_role" "github_actions" {
         Condition = {
           StringEquals = {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
-          }
-          StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:${var.github_repository}:*"
+            "token.actions.githubusercontent.com:sub" = "repo:${var.github_repository}:ref:refs/heads/main"
           }
         }
       }
